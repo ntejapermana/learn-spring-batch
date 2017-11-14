0abd3053e0acd9f8cb336a942e45075cc9513e2c
ntejapermana <nugraha.tejapermana@gdplabs.id>
2017-11-14 19:05:29 +0700

    Custom Job Launcher with multithread support

---
+++ src/main/java/id/duskclouds/springbatch/configuration/CustomJobLauncher.java
@@ -0,0 +1,83 @@
+package id.duskclouds.springbatch.configuration;
+
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
+import org.springframework.batch.core.configuration.support.JobRegistryBeanPostProcessor;
+import org.springframework.batch.core.converter.DefaultJobParametersConverter;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.JobOperator;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobOperator;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+
+@Configuration
+public class CustomJobLauncher extends DefaultBatchConfigurer implements ApplicationContextAware {
+
+    @Autowired
+    public JobExplorer jobExplorer;
+
+    @Autowired
+    public JobRepository jobRepository;
+
+    @Autowired
+    public JobRegistry jobRegistry;
+
+    @Autowired
+    public JobLauncher jobLauncher;
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    public JobRegistryBeanPostProcessor jobRegistrar() throws Exception {
+        JobRegistryBeanPostProcessor registrar = new JobRegistryBeanPostProcessor();
+
+        registrar.setJobRegistry(this.jobRegistry);
+        registrar.setBeanFactory(this.applicationContext.getAutowireCapableBeanFactory());
+        registrar.afterPropertiesSet();
+
+        return registrar;
+    }
+
+    @Bean
+    public JobOperator jobOperator() throws Exception {
+        SimpleJobOperator simpleJobOperator = new SimpleJobOperator();
+
+        simpleJobOperator.setJobLauncher(this.jobLauncher);
+        simpleJobOperator.setJobParametersConverter(new DefaultJobParametersConverter());
+        simpleJobOperator.setJobRepository(this.jobRepository);
+        simpleJobOperator.setJobExplorer(this.jobExplorer);
+        simpleJobOperator.setJobRegistry(this.jobRegistry);
+
+        simpleJobOperator.afterPropertiesSet();
+
+        return simpleJobOperator;
+    }
+
+    @Override
+    public JobLauncher getJobLauncher() {
+        SimpleJobLauncher jobLauncher = null;
+        try {
+            jobLauncher = new SimpleJobLauncher();
+            jobLauncher.setJobRepository(jobRepository);
+            jobLauncher.setTaskExecutor(new SimpleAsyncTaskExecutor());
+            jobLauncher.afterPropertiesSet();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return jobLauncher;
+    }
+}
